3efd0721b1e244b5e0f0672f257da571ec4fe4db
HORI-Tomy <white_clover@hotmail.com>
2020-04-27 22:52:47 +0900

    my first commit

---
+++ 1-6.py
@@ -0,0 +1,12 @@
+age = 5
+
+if age < 20:
+  print("未成年です")
+elif age >= 20 and age < 30:
+  print("若者です")
+elif age >= 30 and age < 50:
+  print("中年です")
+elif age >= 50 and age < 70:
+  print("初老です")
+elif age >= 70:
+  print("老人です")
